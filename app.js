ba3863b809215c626cdebc4f56467f42de623fe5
mchwebdev <36421426+mchwebdev@users.noreply.github.com>
2018-03-02 11:26:47 +0100

    Add firebase

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyCGpPniD4D_FTEUo5Uy4GcRmiWUzBUYNto',
+  authDomain: 'veggie-playit.firebaseapp.com',
+  databaseURL: 'https://veggie-playit.firebaseio.com',
+  projectId: 'veggie-playit',
+  storageBucket: 'veggie-playit.appspot.com',
+  messagingSenderId: '346733121960',
+};
+firebase.initializeApp(config);
